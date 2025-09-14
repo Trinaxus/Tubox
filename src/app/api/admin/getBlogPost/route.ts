666f86d365dc129bbece95ecc625e6e84af27875
Trinaxus <trinaxus@googlemail.com>
2025-09-14 18:56:54 +0200

    fix(build): resolve '@/lib/blogData' using relative path for Vercel

--- src/app/api/admin/getBlogPost/route.ts
+++ src/app/api/admin/getBlogPost/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { fetchBlogPostById } from '@/lib/blogData';
+import { fetchBlogPostById } from '../../../../lib/blogData';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
